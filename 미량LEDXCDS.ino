564847795e096999e1b9c04ecfa7956e29191999
길이스타트업 <51491577+gilitechmaster@users.noreply.github.com>
2021-03-13 23:10:25 +0900

    Update 미량LEDXCDS.ino

--- 미량LEDXCDS.ino
+++ 미량LEDXCDS.ino
@@ -14,8 +14,10 @@ void setup(){
 }
 
 void loop(){
-  analogWrite(inled, 1);
+  analogWrite(inled, 5);
   //LED를 미량만 방출한다.
+  //Capacitive에 2000이 나오므로
+  //2500~1500 사이만 LED on
    CSread();
 }
 
@@ -23,44 +25,17 @@ void CSread(){
   unsigned long cs = cs_2_3.capacitiveSensor(50);
   cs_2_3.set_CS_AutocaL_Millis(50);
   cs_2_3.set_CS_Timeout_Millis(50);
-  
-  if(cs > 1 && cs < 100){
-    Serial.print("수신전류 = 15㎂ ~ 270㎂, 작동저항 = 300k옴 이하, Capacitive = ");
-    Serial.println(cs);
-    digitalWrite(outled, HIGH);
-  }
-  if(cs > 100 && cs < 350){
-    Serial.print("수신전류 = 1.5㎂ ~ 15㎂, 작동저항 = 300k ~ 1M옴, Capacitive = ");
-    Serial.println(cs);
-    digitalWrite(outled, HIGH);
-  }
-  if(cs > 350 && cs < 3300){
-    Serial.print("수신전류 = 150㎁ ~ 1.5㎂, 작동저항 = 1M옴 ~ 10M옴, Capacitive = ");
-    Serial.println(cs);
-    digitalWrite(outled, HIGH);
-  }
-  if(cs > 3300 && cs < 6400){
-    Serial.print("수신전류 = 150㎁ 이하, 작동저항 = 10M옴 ~ 20M옴, Capacitive = ");
-    Serial.println(cs);
-    digitalWrite(outled, HIGH);
-  }
-  if(cs > 6400 && cs < 11000){
-    Serial.print("수신전류 = 150㎁ 이하, 작동저항 = 20M옴 ~ 30M옴, Capacitive = ");
-    Serial.println(cs);
-    digitalWrite(outled, HIGH);
-  }
-  if(cs > 11000 && cs < 15000){
-    Serial.print("수신전류 = 150㎁ 이하, 작동저항 = 30M옴 ~ 40M옴, Capacitive = ");
-    Serial.println(cs);
-    digitalWrite(outled, HIGH);
-  }
-  if(cs > 15000){
-    Serial.print("수신전류 = 오버로드, 작동저항 = 50M옴 이상, Capacitive = ");
-    Serial.println(cs);
-    digitalWrite(outled, HIGH);
-  }
-    
+
+  Serial.println(cs);
+
+  //if(cs > 1 && cs < 2000){
+    //digitalWrite(outled, HIGH);
+  //}
+    if(cs > 1){
+      digitalWrite(outled, LOW);
+    }
+     
     delay(100);
   
-  digitalWrite(outled, LOW);
+  digitalWrite(outled, HIGH);
 }

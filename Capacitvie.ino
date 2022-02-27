5956565833cbf5fbf092eaa637f5bffdd0852b84
길이스타트업 <51491577+gilitechmaster@users.noreply.github.com>
2022-02-27 20:41:38 +0900

    Update Capacitvie.ino

--- Capacitvie.ino
+++ Capacitvie.ino
@@ -11,10 +11,22 @@ void loop(){
 }
 
 void CSread(){
-  unsigned long cs = cs_7_5.capacitiveSensor(55);
+  unsigned long cs = cs_7_5.capacitiveSensor(50);
   cs_7_5.set_CS_AutocaL_Millis(50);
-  cs_7_5.set_CS_Timeout_Millis(50);
+  cs_7_5.set_CS_Timeout_Millis(500); // 50 = 1.5만 OL, 500 = 15만 OL
   if(cs > 1){
     Serial.println(cs);
   }
+  delay(50);
 }
+
+// 42억 = 오버로드 (OL)
+// 15000 = 40M옴
+// 11000 = 30M옴
+// 6400 = 20M옴
+// 3300 = 10M옴
+// 700 = 2M옴
+// 350 = 1M옴
+// 100 = 300k옴
+// 0 = 100k옴
+// 0 = 무저항

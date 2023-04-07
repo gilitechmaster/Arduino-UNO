8d691e3a40ae06c622f43a0309e6f07e9f45de4a
경섭 <51491577+gilitechmaster@users.noreply.github.com>
2023-04-07 18:22:52 +0900

    Update CapacitvieXPWM.ino

--- CapacitvieXPWM.ino
+++ CapacitvieXPWM.ino
@@ -2,12 +2,13 @@
 #include <PWM.h>
 
 int32_t frequency1 = 10; //10헤르츠
-CapacitiveSensor cs_7_5 = CapacitiveSensor(7, 5); //send, receive
-
+CapacitiveSensor cs_12_5 = CapacitiveSensor(12, 5); //send, receive
+//PWM이 마이크로 암페어라서 합선될 수 있으므로 핀7과 핀12는 멀리한다.
 
 void setup(){
   //InitTimerSafe();
-  bool success1 = SetPinFrequencySafe(9, frequency1);
+  //bool success1 = SetPinFrequencySafe(9, frequency1);
+  //SetPinFrequencySafe는 주파수 세팅인데 필요한지 모르겠다.
   Serial.begin(9600);
 }
 
@@ -21,9 +22,9 @@ void loop(){
 }
 
 void CSread(){
-  unsigned long cs = cs_7_5.capacitiveSensor(50);
-  cs_7_5.set_CS_AutocaL_Millis(50);
-  cs_7_5.set_CS_Timeout_Millis(50);
+  unsigned long cs = cs_12_5.capacitiveSensor(50);
+  cs_12_5.set_CS_AutocaL_Millis(50);
+  cs_12_5.set_CS_Timeout_Millis(50);
   if(cs > 1){
     Serial.println(cs);
     delay(50);

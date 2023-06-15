c134719d0cecfd7a989d07f3fa7a83d5ec9b45bc
경섭 <51491577+gilitechmaster@users.noreply.github.com>
2023-06-15 22:41:31 +0900

    Update 적외선센서.ino

--- 적외선센서.ino
+++ 적외선센서.ino
@@ -1,24 +1,14 @@
 #define inLED A0 //수광부
 #define outLED 13 //발광부
 
-int val = 0;
-void setup() {
+void setup(){
   pinMode(outLED, OUTPUT);
   Serial.begin(9600);
 }
 
 void loop() {
   int r = analogRead(inLED);
-  if(r > 999){
-    digitalWrite(outLED, HIGH);
-  }else{
-    digitalWrite(outLED, LOW);
-  }
-  if( abs(r - val) > 0){
-    Serial.println(r);
-    val = r;
-    delay(100);
-  }
+  Serial.println(r);
+  digitalWrite(outLED, LOW);
+  delay(100);
 }
-
-//https://blog.xcoda.net/78

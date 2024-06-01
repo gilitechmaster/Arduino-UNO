91b634f9a801c192a488f9fa49c56d7e6c043d6c
경섭 <51491577+gilitechmaster@users.noreply.github.com>
2024-06-02 00:23:21 +0900

    Update analogRead_적외선센서_터치.ino

--- analogRead_적외선센서_터치.ino
+++ analogRead_적외선센서_터치.ino
@@ -22,14 +22,14 @@ void CSread(){
   cs_2_3.set_CS_Timeout_Millis(50);
         if(cs1 > 14000){
         analogWrite(outLED, 1);
-        //LED를 미량만 방출한다.
+        //적외선을 미량만 방출한다.
       
     }
     if(cs1 < 14000){
         analogWrite(outLED, 0);
-        //터치하면 LED 꺼짐
+        //터치하면 적외선 꺼짐
     }
 }
 
-//터치할 때 9~120
-//터치 안할 때 0~130
+//터치할 때(off) 9~120
+//터치 안할 때(on) 0~130
